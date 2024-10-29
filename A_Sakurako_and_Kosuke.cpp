5757f0dffe157ed786dffafbc549d389c5880f82
Mashrafi Mahin <mashrafimahin383@gmail.com>
2024-10-30 00:03:44 +0600

    fix the sakurako soln

--- A_Sakurako_and_Kosuke.cpp
+++ A_Sakurako_and_Kosuke.cpp
@@ -1,58 +1,21 @@
 #include <iostream>
-
+using namespace std;
 int main(){
     int t;
-    int n;
-    do
+    cin>>t;
+    int z;
+    for (size_t i = 0; i < t; i++)
     {
-    std::cin>>t;
-    } while (t<1 ^ t>100);
-
-        do
-    {
-    std::cin>>n;
-    } while (n<1 ^ n>100);
-
-    bool toggle = true;//true for sakura
-    int x = 0;
-    for (int i = 0; i < t; i++)
-    {
-    if (x>=0)
-    {
-        if (x>n){
-            break;
-        }
-    }
-    else if(x<0)
-    {
-        if (x<(-n)){
-            break;
-        }
-    }
-    if (i%2==0)
-        {
-            x-= (2*i-1);
-            toggle = true;
-        }
-    else
-        {
-            x+= (2*i-1);
-            toggle = false;
-
-        }
-        
- }
-
-    if (toggle==true){
-        std::cout<<"Sakurako"<<'/n';
-    }
-
-    else if (toggle==false){
-        std::cout<<"Kosuke"<<'/n';
-    }
-
-
+        int z;
+        cin>>z;
+        if (z %2 ==0)
+{
+    cout<<"Sakurako"<<endl;
+}
+else
+    cout<<"Kosuke"<<endl;
     
-    
-    
+    }
+    return 0;
+
 }
